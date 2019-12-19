ebff3859cbf1f964e4c87c08cacdc0f7510bdae6
Devon <Dvowen@cox.net>
2019-12-18 23:27:25 -0800

    Fixed security dependency?

--- server/app.js
+++ server/app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var port = process.env.PORT || 3001;
 var indexRouter = require('./routes/index');
@@ -9,11 +7,8 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/api', indexRouter);
